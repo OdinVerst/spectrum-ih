eaff53954b9253577e987709353690c1a10e7eba
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-08-13 00:10:24 +0300

    Chenge img, optimaze

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import customScrollTo from './smoothScroll';
 
+let position = window.scrollY;
+
 const allLinks = document.querySelectorAll('.nav__item-link');
 if (allLinks.length) {
 	[...allLinks].forEach(link => {
@@ -12,3 +14,90 @@ if (allLinks.length) {
 		});
 	});
 }
+
+const easeBoxes = [];
+const cristals = document.querySelectorAll('.main__content');
+if (cristals.length) {
+	[...cristals].forEach(cristal => {
+		easeBoxes.push(
+			basicScroll.create({
+				elem: cristal,
+				from: 'middle-bottom',
+				to: 'bottom-top',
+				direct: document.querySelector('.main__content'),
+				props: {
+					'--ty': {
+						from: '0',
+						to: '150px',
+						timing: 'expoIn'
+					}
+				}
+			})
+		);
+	});
+}
+
+//easeBoxes.forEach(easeBox => easeBox.start());
+
+// instance.start();
+
+// const instance2 = basicScroll.create({
+// 	elem: document.querySelector('.cristal-2'),
+// 	from: 'bottom-top',
+// 	to: 'top-bottom',
+// 	direct: true,
+// 	props: {
+// 		'--translateY': {
+// 			from: '0',
+// 			to: '30%',
+// 			timing: 'elasticOut'
+// 		}
+// 	}
+// });
+// const instance3 = basicScroll.create({
+// 	elem: document.querySelector('.cristal-3'),
+// 	from: 'bottom-top',
+// 	to: 'top-bottom',
+// 	direct: true,
+// 	props: {
+// 		'--translateY': {
+// 			from: '0',
+// 			to: '30%',
+// 			timing: 'elasticOut'
+// 		}
+// 	}
+// });
+// const instance4 = basicScroll.create({
+// 	elem: document.querySelector('.cristal-4'),
+// 	from: 'bottom-top',
+// 	to: 'top-bottom',
+// 	direct: true,
+// 	props: {
+// 		'--translateY': {
+// 			from: '0',
+// 			to: '30%',
+// 			timing: 'elasticOut'
+// 		}
+// 	}
+// });
+
+// instance2.start();
+// instance3.start();
+// instance4.start();
+
+// document.addEventListener('scroll', () => {
+// 	const crictal = document.querySelector('.cristal-1');
+// 	const crictal2 = document.querySelector('.cristal-2');
+// 	var t = performance.now();
+// 	//console.log(position - window.scrollY);
+// 	if (Math.abs(position - window.scrollY) < 70) {
+// 		crictal.style.marginTop = -((position - window.scrollY) / 2) + 'px';
+// 		crictal2.style.marginBottom = (position - window.scrollY) / 2 + 'px';
+// 		//crictal2.style.transform = `rotate(${(position - window.scrollY) / 2}deg)`;
+// 		(position - window.scrollY) / 2 + 'px';
+// 	}
+// 	//console.log(window.screenTop / t);
+// 	// if (window.scrollY < 150) {
+// 	// 	crictal.style.marginTop = -(window.scrollY / 2) + 'px';
+// 	// }
+// });
